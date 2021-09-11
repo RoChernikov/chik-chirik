9bf2ef46e2e796dff7cd3a3af6b6b7d9bd3c5edd
Ro Chernikov <imrhubris@gmail.com>
2021-09-12 01:13:27 +0300

    feat: sliders added

--- js/script.js
+++ js/script.js
@@ -9,3 +9,41 @@ detailsBtn.onclick = () => detailsInfo.scrollIntoView({
   behavior: "smooth"
 });
 // ---------------------------------------------------
+
+// --+++Инициализация Swiper JS для слайдера с карточками+++--
+const textCardsSlider = new Swiper('.slider-cards', {
+  // Параметры
+  wrapperClass: 'slider-cards__wrapper',
+  slideClass: 'slider-cards__item',
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 20,
+
+  // Пагинация
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__bullet',
+    bulletActiveClass: 'pagination__bullet_active',
+    clickable: false,
+  },
+});
+// -----------------------------------------------------------
+
+// --+++Инициализация Swiper JS для слайдера с картинками+++--
+const imageCardsSlider = new Swiper('.slider-img', {
+  // Параметры
+  wrapperClass: 'slider-img__wrapper',
+  slideClass: 'slider-img__item',
+  direction: 'horizontal',
+  loop: false,
+  spaceBetween: 60,
+
+  // Пагинация
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__bullet',
+    bulletActiveClass: 'pagination__bullet_active',
+    clickable: false,
+  },
+});
+// ------------------------------------------------------------
